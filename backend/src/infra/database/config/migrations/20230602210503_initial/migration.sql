64b46192519a7f6af4004239b48f21bdf2745821
Pablo Pessanha <pablopessanh@gmail.com>
2023-06-02 19:15:55 -0300

    feat: dockerizing application

--- backend/src/infra/database/config/migrations/20230602210503_initial/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Branches" (
-    "id" UUID NOT NULL,
-    "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-
-    CONSTRAINT "Branches_pkey" PRIMARY KEY ("id")
-);
