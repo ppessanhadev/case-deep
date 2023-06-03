222a197359afa105455c82207bad3ec373890b8e
Pablo Pessanha <pablopessanh@gmail.com>
2023-06-03 16:27:53 -0300

    feat: creates computer repository

--- backend/src/infra/repositories/repositories.module.ts
+++ backend/src/infra/repositories/repositories.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@infra/database/database.module';
 import { BranchRepository } from '@infra/repositories/branch.repository';
 import { AirConditionerRepository } from '@infra/repositories/air-conditioner.repository';
+import { ComputerRepository } from '@infra/repositories/computer.repository';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [BranchRepository, AirConditionerRepository],
-  exports: [BranchRepository, AirConditionerRepository],
+  providers: [BranchRepository, AirConditionerRepository, ComputerRepository],
+  exports: [BranchRepository, AirConditionerRepository, ComputerRepository],
 })
 export class RepositoriesModule {}
