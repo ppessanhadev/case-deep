6278412975154222f715e0d05ca8d72705502151
Pablo Pessanha <pablopessanh@gmail.com>
2023-06-04 01:12:16 -0300

    refactor: removes unnecessary env config setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { ControllerModule } from '@application/controllers/controller.module';
 
 @Module({
-  imports: [
-    ControllerModule,
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
-    }),
-  ],
+  imports: [ControllerModule],
 })
 export class AppModule {}
