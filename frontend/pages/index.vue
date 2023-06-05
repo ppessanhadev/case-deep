81fd3a71ab37437b682653750a7c540fd2a53c7b
Pablo Pessanha <pablopessanh@gmail.com>
2023-06-04 21:52:47 -0300

    feat: adds create button on branches page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -5,9 +5,21 @@
 <template>
   <main class="w-full h-screen">
     <Navbar />
-    <h1 class="block text-3xl text-slate-700 font-bold w-[80%] mx-auto pt-24 pb-2">
-      Afiliados
-    </h1>
+
+    <section class="flex justify-between items-center w-[80%] mx-auto pt-24 pb-2">
+      <h1 class="block text-3xl text-slate-700 font-bold ">
+        Afiliados
+      </h1>
+
+      <button
+        type="button"
+        class="block-inline border-2 border-green-500 rounded-lg hover:bg-green-500 hover:text-white transition-all text-green-500 font-bold p-2 disabled:opacity-70 disabled:cursor-not-allowed"
+      >
+        <p class="p-1 text-2xl">
+          Criar Afiliado
+        </p>
+      </button>
+    </section>
 
     <BranchTable />
   </main>
