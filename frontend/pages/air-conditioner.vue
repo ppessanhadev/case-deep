9df77e5759e9ff57e9b70cec003c1474bc5b2340
Pablo Pessanha <pablopessanh@gmail.com>
2023-06-05 18:12:22 -0300

    feat: adds handle creation modal on air conditioner page

--- frontend/pages/air-conditioner.vue
+++ frontend/pages/air-conditioner.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
   useHead({ title: 'DEEPESG | Ares condicionados' });
+
+  const create = ref(false);
+
+  const handleCreate = () => {
+    create.value = !create.value;
+  };
 </script>
 
 <template>
   <main class="w-full h-screen">
     <Navbar />
 
-    <Header title="Ar condicionado" title-plural="Ares Condicionados" />
+    <Header title="Ar condicionado" title-plural="Ares Condicionados" :open="handleCreate" />
+
+    <ModalFormAirConditioner v-if="create" :close="handleCreate" />
 
     <AirConditionerTable />
   </main>
