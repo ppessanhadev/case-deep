ad49309defc01dba81da62ae5643fcecbcc000f7
Pablo Pessanha <pablopessanh@gmail.com>
2023-06-05 11:54:26 -0300

    feat: adds heade table component for all pages

---
+++ frontend/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  defineProps<{ title: string, titlePlural: string }>();
+</script>
+
+<template>
+  <header class="flex justify-between items-center w-[80%] mx-auto pt-24 pb-2">
+    <h1 class="block text-3xl text-slate-700 font-bold capitalize">
+      {{ titlePlural }}
+    </h1>
+
+    <button type="button" class="btn-create capitalize">
+      <p class="p-1 text-2xl">
+        Criar {{ title }}
+      </p>
+    </button>
+  </header>
+</template>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -6,17 +6,7 @@
   <main class="w-full h-screen">
     <Navbar />
 
-    <section class="flex justify-between items-center w-[80%] mx-auto pt-24 pb-2">
-      <h1 class="block text-3xl text-slate-700 font-bold ">
-        Afiliados
-      </h1>
-
-      <button type="button" class="btn-create">
-        <p class="p-1 text-2xl">
-          Criar Afiliado
-        </p>
-      </button>
-    </section>
+    <Header title="Afiliado" title-plural="Afiliados" />
 
     <BranchTable />
   </main>
